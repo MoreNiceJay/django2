06dbf19c0aeea92360bf5e3b1b5ac79a018f6f9f
Janghyup Lee <morenicejay@gmail.com>
2017-06-04 20:30:42 +0900

    lec 13

--- music/views.py
+++ music/views.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from .models import Album
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app homepage</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/'+ str(album.id) +'/'
+        html += '<a href="'+ url + '">'+ album.album_title + '</a><br>'
+        #print "########" + html
+    return HttpResponse(html)
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
